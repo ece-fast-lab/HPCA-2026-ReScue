982827dd84c6e42ce891297e5c2c72b91707b057
chihuns2 <94077406+chihuns2@users.noreply.github.com>
2025-12-06 09:15:45 -0600

    Update run_spec_eq.sh

--- AE23/run_spec/run_spec_eq.sh
+++ AE23/run_spec/run_spec_eq.sh
@@ -7,5 +7,5 @@ source shrc
 cd "$CUR_DIR"
 
 echo "Running SEPC2017"
-sudo numactl -N 0 -m 1 /research/chihun/bench/spec2017/bin/runcpu --noreportable --size ref --tuning base --iteration 2 --action onlyrun --config myprogram-gcc-linux-x86.cfg --copies=8 548 525 502  > eq_spec_int.lis
+sudo numactl -N 0 -m 1 /research/chihun/bench/spec2017/bin/runcpu --noreportable --size ref --tuning base --iteration 1 --action onlyrun --config myprogram-gcc-linux-x86.cfg --copies=8 548 525 502  > eq_spec_int.lis
 
